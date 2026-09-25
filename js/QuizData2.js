125180e53cc4c1d3e06a3eb47d3f999487eec659
kikuchiRBC <kikuchi@rbc.ac.jp>
2026-09-25 11:04:27 +0900

    Add files via upload

---
+++ js/QuizData2.js
@@ -0,0 +1,66 @@
+const sortQuizData = [
+
+    {
+        question:"トイレ<ruby>清掃<rt>せいそう</rt></ruby>の<ruby>順番<rt>じゅんばん</rt></ruby>をえらびなさい",
+
+        images:[
+            "images/toire1.png",
+            "images/toire2.png",
+            "images/toire3.png",
+            "images/toire4.png"
+        ],
+
+        answer:[3,2,0,1]
+    }
+,
+
+    {
+        question:"<ruby>床清掃<rt>ゆかせいそう</rt></ruby>の<ruby>順番<rt>じゅんばん</rt></ruby>をえらびなさい",
+
+        images:[
+            "images/yuka1.png",
+            "images/yuka2.png",
+            "images/yuka3.png",
+            "images/yuka4.png"
+        ],
+
+        answer:[0,2,3,1]
+    }
+    ,
+        {
+        question:"ガラス<ruby>清掃<rt>せいそう</rt></ruby>の<ruby>順番<rt>じゅんばん</rt></ruby>をえらびなさい",
+
+        images:[
+            "images/garasu1.png",
+            "images/garasu2.png",
+            "images/garasu3.png",
+            "images/garasu4.png"
+        ],
+
+        answer:[0,1,3,2]
+    },
+            {
+        question:"<ruby>除塵作業<rt>じょじんさぎょう</rt></ruby>の<ruby>順番<rt>じゅんばん</rt></ruby>をえらびなさい",
+
+        images:[
+            "images/jojin1.png",
+            "images/jojin2.png",
+            "images/jojin3.png",
+            "images/jojin4.png"
+        ],
+
+        answer:[2,0,3,1]
+    },
+                {
+        question:"モップ<ruby>清掃<rt>せいそう</rt></ruby>の<ruby>順番<rt>じゅんばん</rt></ruby>をえらびなさい",
+
+        images:[
+            "images/mop1.png",
+            "images/mop2.png",
+            "images/mop3.png",
+            "images/mop4.png"
+        ],
+
+        answer:[2,3,0,1]
+    }
+];
